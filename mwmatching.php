3f2f0d94406b7b7120299f1e1d33c008dafad38e
Thomas David Baker <bakert@gmail.com>
2021-09-09 07:45:41 -0700

    Assert correctly in tests

--- mwmatching.php
+++ mwmatching.php
@@ -1129,96 +1129,96 @@ if ($argv && $argv[0] && realpath($argv[0]) === __FILE__) {
 
         function test11_singleedge() {
             # single edge
-            assert(maxWeightMatching([[0, 1, 1]]), [1, 0]);
+            assert(maxWeightMatching([[0, 1, 1]]) === [1, 0]);
         }
 
        function test12() {
-            assert(maxWeightMatching([[1, 2, 10], [2, 3, 11]]), [-1, -1, 3, 2]);
+            assert(maxWeightMatching([[1, 2, 10], [2, 3, 11]]) === [-1, -1, 3, 2]);
         }
 
         function test13() {
-            assert(maxWeightMatching([[1, 2, 5], [2, 3, 11], [3, 4, 5]]), [-1, -1, 3, 2, -1]);
+            assert(maxWeightMatching([[1, 2, 5], [2, 3, 11], [3, 4, 5]]) === [-1, -1, 3, 2, -1]);
         }
 
 
         function test14_maxcard() {
             # maximum cardinality
-            assert(maxWeightMatching([[1, 2, 5], [2, 3, 11], [3, 4, 5]], true), [-1, 2, 1, 4, 3]);
+            assert(maxWeightMatching([[1, 2, 5], [2, 3, 11], [3, 4, 5]], true) === [-1, 2, 1, 4, 3]);
         }
 
         function test15_float() {
             # floating point weigths
-            assert(maxWeightMatching([[1, 2, M_PI], [2, 3, exp(1)], [1, 3, 3.0], [1, 4, sqrt(2.0)]]), [-1, 4, 3, 2, 1]);
+            assert(maxWeightMatching([[1, 2, M_PI], [2, 3, exp(1)], [1, 3, 3.0], [1, 4, sqrt(2.0)]]) === [-1, 4, 3, 2, 1]);
         }
 
         function test16_negative() {
             # negative weights
-            assert(maxWeightMatching([[1, 2, 2], [1, 3, -2], [2, 3, 1], [2, 4, -1], [3, 4, -6]], false), [-1, 2, 1, -1, -1]);
-            assert(maxWeightMatching([[1, 2, 2], [1, 3, -2], [2, 3, 1], [2, 4, -1], [3, 4, -6]], true), [-1, 3, 4, 1, 2]);
+            assert(maxWeightMatching([[1, 2, 2], [1, 3, -2], [2, 3, 1], [2, 4, -1], [3, 4, -6]], false) === [-1, 2, 1, -1, -1]);
+            assert(maxWeightMatching([[1, 2, 2], [1, 3, -2], [2, 3, 1], [2, 4, -1], [3, 4, -6]], true) === [-1, 3, 4, 1, 2]);
         }
 
 
         function test20_sblossom() {
             # create S-blossom && use it for augmentation
-            assert(maxWeightMatching([[1, 2, 8], [1, 3, 9], [2, 3, 10], [3, 4, 7]]), [-1, 2, 1, 4, 3]);
-            assert(maxWeightMatching([[1, 2, 8], [1, 3, 9], [2, 3, 10], [3, 4, 7], [1, 6, 5], [4, 5, 6]]), [-1, 6, 3, 2, 5, 4, 1]);
+            assert(maxWeightMatching([[1, 2, 8], [1, 3, 9], [2, 3, 10], [3, 4, 7]]) === [-1, 2, 1, 4, 3]);
+            assert(maxWeightMatching([[1, 2, 8], [1, 3, 9], [2, 3, 10], [3, 4, 7], [1, 6, 5], [4, 5, 6]]) === [-1, 6, 3, 2, 5, 4, 1]);
         }
 
         function test21_tblossom() {
             # create S-blossom, relabel as T-blossom, use for augmentation
-            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 4], [1, 6, 3]]), [-1, 6, 3, 2, 5, 4, 1]);
-            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 3], [1, 6, 4]]), [-1, 6, 3, 2, 5, 4, 1]);
-            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 3], [3, 6, 4]]), [-1, 2, 1, 6, 5, 4, 3]);
+            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 4], [1, 6, 3]]) === [-1, 6, 3, 2, 5, 4, 1]);
+            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 3], [1, 6, 4]]) === [-1, 6, 3, 2, 5, 4, 1]);
+            assert(maxWeightMatching([[1, 2, 9], [1, 3, 8], [2, 3, 10], [1, 4, 5], [4, 5, 3], [3, 6, 4]]) === [-1, 2, 1, 6, 5, 4, 3]);
         }
 
         function test22_s_nest() {
             # create nested S-blossom, use for augmentation
-            assert(maxWeightMatching([[1, 2, 9], [1, 3, 9], [2, 3, 10], [2, 4, 8], [3, 5, 8], [4, 5, 10], [5, 6, 6]]), [-1, 3, 4, 1, 2, 6, 5]);
+            assert(maxWeightMatching([[1, 2, 9], [1, 3, 9], [2, 3, 10], [2, 4, 8], [3, 5, 8], [4, 5, 10], [5, 6, 6]]) === [-1, 3, 4, 1, 2, 6, 5]);
         }
 
         function test23_s_relabel_nest() {
             # create S-blossom, relabel as S, include in nested S-blossom
-            assert(maxWeightMatching([[1, 2, 10], [1, 7, 10], [2, 3, 12], [3, 4, 20], [3, 5, 20], [4, 5, 25], [5, 6, 10], [6, 7, 10], [7, 8, 8]]), [-1, 2, 1, 4, 3, 6, 5, 8, 7]);
+            assert(maxWeightMatching([[1, 2, 10], [1, 7, 10], [2, 3, 12], [3, 4, 20], [3, 5, 20], [4, 5, 25], [5, 6, 10], [6, 7, 10], [7, 8, 8]]) === [-1, 2, 1, 4, 3, 6, 5, 8, 7]);
         }
 
         function test24_s_nest_expand() {
             # create nested S-blossom, augment, expand recursively
-            assert(maxWeightMatching([[1, 2, 8], [1, 3, 8], [2, 3, 10], [2, 4, 12], [3, 5, 12], [4, 5, 14], [4, 6, 12], [5, 7, 12], [6, 7, 14], [7, 8, 12]]), [-1, 2, 1, 5, 6, 3, 4, 8, 7]);
+            assert(maxWeightMatching([[1, 2, 8], [1, 3, 8], [2, 3, 10], [2, 4, 12], [3, 5, 12], [4, 5, 14], [4, 6, 12], [5, 7, 12], [6, 7, 14], [7, 8, 12]]) === [-1, 2, 1, 5, 6, 3, 4, 8, 7]);
         }
 
         function test25_s_t_expand() {
             # create S-blossom, relabel as T, expand
-            assert(maxWeightMatching([[1, 2, 23], [1, 5, 22], [1, 6, 15], [2, 3, 25], [3, 4, 22], [4, 5, 25], [4, 8, 14], [5, 7, 13]]), [-1, 6, 3, 2, 8, 7, 1, 5, 4]);
+            assert(maxWeightMatching([[1, 2, 23], [1, 5, 22], [1, 6, 15], [2, 3, 25], [3, 4, 22], [4, 5, 25], [4, 8, 14], [5, 7, 13]]) === [-1, 6, 3, 2, 8, 7, 1, 5, 4]);
         }
 
         function test26_s_nest_t_expand() {
             # create nested S-blossom, relabel as T, expand
-            assert(maxWeightMatching([[1, 2, 19], [1, 3, 20], [1, 8, 8], [2, 3, 25], [2, 4, 18], [3, 5, 18], [4, 5, 13], [4, 7, 7], [5, 6, 7]]), [-1, 8, 3, 2, 7, 6, 5, 4, 1]);
+            assert(maxWeightMatching([[1, 2, 19], [1, 3, 20], [1, 8, 8], [2, 3, 25], [2, 4, 18], [3, 5, 18], [4, 5, 13], [4, 7, 7], [5, 6, 7]]) === [-1, 8, 3, 2, 7, 6, 5, 4, 1]);
         }
 
         function test30_tnasty_expand() {
             # create blossom, relabel as T in more than one way, expand, augment
-            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 35], [5, 7, 26], [9, 10, 5]]), [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
+            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 35], [5, 7, 26], [9, 10, 5]]) === [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
         }
 
         function test31_tnasty2_expand() {
             # again but slightly different
-            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 26], [5, 7, 40], [9, 10, 5]]), [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
+            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 26], [5, 7, 40], [9, 10, 5]]) === [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
         }
 
         function test32_t_expand_leastslack() {
             # create blossom, relabel as T, expand such that a new least-slack S-to-free edge is produced, augment
-            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 28], [5, 7, 26], [9, 10, 5]]), [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
+            assert(maxWeightMatching([[1, 2, 45], [1, 5, 45], [2, 3, 50], [3, 4, 45], [4, 5, 50], [1, 6, 30], [3, 9, 35], [4, 8, 28], [5, 7, 26], [9, 10, 5]]) === [-1, 6, 3, 2, 8, 7, 1, 5, 4, 10, 9]);
         }
 
         function test33_nest_tnasty_expand() {
             # create nested blossom, relabel as T in more than one way, expand outer blossom such that inner blossom ends up on an augmenting $path
-            assert(maxWeightMatching([[1, 2, 45], [1, 7, 45], [2, 3, 50], [3, 4, 45], [4, 5, 95], [4, 6, 94], [5, 6, 94], [6, 7, 50], [1, 8, 30], [3, 11, 35], [5, 9, 36], [7, 10, 26], [11, 12, 5]]), [-1, 8, 3, 2, 6, 9, 4, 10, 1, 5, 7, 12, 11]);
+            assert(maxWeightMatching([[1, 2, 45], [1, 7, 45], [2, 3, 50], [3, 4, 45], [4, 5, 95], [4, 6, 94], [5, 6, 94], [6, 7, 50], [1, 8, 30], [3, 11, 35], [5, 9, 36], [7, 10, 26], [11, 12, 5]]) === [-1, 8, 3, 2, 6, 9, 4, 10, 1, 5, 7, 12, 11]);
         }
 
         function test34_nest_relabel_expand() {
             # create nested S-blossom, relabel as S, expand recursively
-            assert(maxWeightMatching([[1, 2, 40], [1, 3, 40], [2, 3, 60], [2, 4, 55], [3, 5, 55], [4, 5, 50], [1, 8, 15], [5, 7, 30], [7, 6, 10], [8, 10, 10], [4, 9, 30]]), [-1, 2, 1, 5, 9, 3, 7, 6, 10, 4, 8]);
+            assert(maxWeightMatching([[1, 2, 40], [1, 3, 40], [2, 3, 60], [2, 4, 55], [3, 5, 55], [4, 5, 50], [1, 8, 15], [5, 7, 30], [7, 6, 10], [8, 10, 10], [4, 9, 30]]) === [-1, 2, 1, 5, 9, 3, 7, 6, 10, 4, 8]);
         }
     }
 
